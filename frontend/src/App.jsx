6cf3dd4b58457497f137a311ad4f72cc85424a62
PraveenKumarM17 <kanniymma@gmail.com>
2026-02-08 14:35:22 +0530

    set window to scroll from top

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
+import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Members from "./pages/Members";
 import MainLayout from "./layout/MainLayout";
 
 export default function App() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <MainLayout>
       <Routes>
